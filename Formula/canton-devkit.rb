30850d1c8ce13d20577798b209b6be3f24b669df
Zhe Li <zhe@gateway.fm>
2026-07-12 12:52:39 +0000

    chore: bump Homebrew formula to v0.15.4

--- Formula/canton-devkit.rb
+++ Formula/canton-devkit.rb
@@ -2,19 +2,19 @@ class CantonDevkit < Formula
   desc "Canton DevKit: LocalNet, DAR, contracts, and observability tooling"
   homepage "https://github.com/bitdynamics-ab/canton-devkit"
   license "Apache-2.0"
-  version "0.15.3"
+  version "0.15.4"
 
   on_macos do
     on_arm do
       url "https://github.com/bitdynamics-ab/canton-devkit/releases/download/v#{version}/canton-devkit_v#{version}_darwin_arm64.tar.gz"
-      sha256 "08d358164c82ec6003d28dd6b2b4cdb7ba3321e7b3b51ef1090e0d8e89c6f8cc"
+      sha256 "ff25bde38dfcd0290a29c1d3569b2ac72c14397e4aa80edcca2778be2d4bca1e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/bitdynamics-ab/canton-devkit/releases/download/v#{version}/canton-devkit_v#{version}_linux_amd64.tar.gz"
-      sha256 "5f49883f7b4db9f1da4818f2717f4f7001d80810081a801674c8bf8167d1a12e"
+      sha256 "6207d2a88ce7115359d3b3b80895e4e57764de1271edf140512228f2f2b2f5fb"
     end
   end
 
